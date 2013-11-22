f2f5c808b8ba49f2a4d7a502b6df7770c93a6750
patrick <patrick.marschallek@gmail.com>
2013-11-22 09:52:17 +0100

    balancing parantheses solved

--- beleg1/Klammerung.scala
+++ beleg1/Klammerung.scala
@@ -2,5 +2,25 @@ package beleg1
 
 object Klammerung {
 
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    var openBraces = 0
+    def traverse(list: List[Char]): Boolean = {
+      if (list.isEmpty)
+        openBraces == 0
+      else if (openBraces < 0)
+        false
+      else {
+        check(list.head)
+        traverse(list.tail)
+      }
+    }
+
+    def check(head: Char) = {
+      if (head == '(')
+        openBraces = openBraces + 1
+      else if (head == ')')
+        openBraces = openBraces - 1
+    }
+    traverse(chars)
+  }
 }
